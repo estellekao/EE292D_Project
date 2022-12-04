a1c1fe53f741bc163ee7af25b2efa7791763df8a
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-12-04 15:14:25 -0800

    Update fall_prediction.py

--- UpBoard/fall_prediction.py
+++ UpBoard/fall_prediction.py
@@ -11,7 +11,10 @@ import joblib
 from sklearn.neural_network import MLPClassifier
 import serial
 import os
+import math
 #import uselect
+
+
 
 
 def read_serial_input():
@@ -93,14 +96,86 @@ def get_acc_data():
     #xAccl = xAccl *2*9.8/32767
     #yAccl = yAccl *2*9.8/32767
     #zAccl = zAccl *2*9.8/32767
+
+    #Convert to mobi fall format (TODO uncomment)
+    xAccl = (xAccl*9.80665)/(16666.6)
+    yAccl = (yAccl*9.80665)/(16666.6)
+    zAccl = (zAccl*9.80665)/(16666.6)
+
     return xAccl, yAccl, zAccl
+
+
+def get_gyro_data():
+    # Get I2C bus
+    bus = smbus.SMBus(1)
+
+
+    gyro_address = 106
+    gyro_reg = 32
+
+    #i2c_bus.write_byte_data(0x6A, 0x20, 0x0F)
+
+    bus.write_byte_data(gyro_address,gyro_reg, 15)
+    
+     # LSM330 Gyro address, 0x6A(106)
+    # Read data back from 0x28(40), 2 bytes
+    # X-Axis Gyro LSB, X-Axis Gyro MSB
+    data0 = bus.read_byte_data(gyro_address, 0x28)
+    data1 = bus.read_byte_data(gyro_address, 0x29)
+
+    # Convert the data
+    xGyro = int((data1 * 256 + data0))
+    if xGyro > 32767 :
+        xGyro -= 65536
+
+    # LSM330 Gyro address, 0x6A(106)
+    # Read data back from 0x2A(42), 2 bytes
+    # Y-Axis Gryo LSB, Y-Axis Gyro MSB
+    data0 = bus.read_byte_data(gyro_address, 0x2A)
+    data1 = bus.read_byte_data(gyro_address, 0x2B)
+
+    # Convert the data
+    yGyro = data1 * 256 + data0
+    if yGyro > 32767 :
+        yGyro -= 65536
+
+    # LSM330 Gyro address, 0x6A(106)
+    # Read data back from 0x2C(44), 2 bytes
+    # Z-Axis Gyro LSB, Z-Axis Gyro MSB
+    data0 = bus.read_byte_data(gyro_address, 0x2C)
+    data1 = bus.read_byte_data(gyro_address, 0x2D)
+
+    # Convert the data
+    zGyro = data1 * 256 + data0
+    if zGyro > 32767 :
+        zGyro -= 65536
+
+
+ 
+
+
+    #Correct for gyro sensitivy and convert to rad/s
+    xGyro = (xGyro*math.pi)/(114.285*180)
+    yGyro = (yGyro*math.pi)/(114.285*180)
+    zGyro = (zGyro*math.pi)/(114.285*180)
+    
+    return xGyro, yGyro, zGyro
+
+
+
+
 
 def get_mult_acc_data(time_interval = 6, data_spacing = 0.005, fake=False):
     x_acc = []
     y_acc = []
     z_acc = []
+    x_gyro = []
+    y_gyro = []
+    z_gyro = []
     for i in range(0,int(time_interval/data_spacing)):
         # repeadly get accelerometer data
+        xAccl, yAccl, zAccl = [0,0,0]
+        xGyro, yGyro, zGyro = [0,0,0]
         if fake:
             xAccl = np.random.randint(20000) - 10000
             yAccl = np.random.randint(20000) - 10000
@@ -108,13 +183,18 @@ def get_mult_acc_data(time_interval = 6, data_spacing = 0.005, fake=False):
             #print([xAccl,yAccl,zAccl])
         else:
             xAccl, yAccl, zAccl = get_acc_data()
+            xGyro, yGyro, zGyro = get_gyro_data()
+
         x_acc.append(xAccl)
         y_acc.append(yAccl)
         z_acc.append(zAccl)
+        x_gyro.append(xGyro)
+        y_gyro.append(yGyro)
+        z_gyro.append(zGyro)
         
         # sleep
         time.sleep(data_spacing)
-    return x_acc, y_acc, z_acc
+    return x_acc, y_acc, z_acc, x_gyro, y_gyro, z_gyro
 
 
 # Calculates the number of times the sample crosses zero
@@ -127,7 +207,124 @@ def min_max_distance(slice):
     return np.sqrt(np.square(np.amax(slice) - np.amin(slice))
                    + (np.square(np.argmax(slice) - np.argmin(slice))))
 
-def preprocess_acc_data(acc_x, acc_y, acc_z):
+
+def calc_attitude_data(acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z):
+    # Initialise matrices and variables
+    #C = np.array([[1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0] ])
+    # P = np.eye(6)
+    # Q = np.eye(6)
+    # R = np.eye(2)
+
+    #state_estimate = np.array([[0], [0], [0], [0], [0], [0]])
+
+    C = np.array([[1, 0, 0, 0], [0, 0, 1, 0]])
+    P = np.eye(4)
+    Q = np.eye(4)
+    R = np.eye(2)
+
+    state_estimate = np.array([[0], [0], [0], [0]])
+
+    phi_hat = 0.0
+    theta_hat = 0.0
+
+  
+
+
+
+    # Calculate accelerometer offsets
+    N = len(gyro_x)
+
+    phi_hat_arr = [0]*N
+    theta_hat_arr  = [0]*N
+    yaw_hat_arr  = [0]*N
+    phi_offset = 0.0
+    theta_offset = 0.0
+
+    for i in range(len(gyro_x)):
+        phi_acc = math.atan2(acc_y[i], math.sqrt(acc_x[i] ** 2.0 + acc_z[i] ** 2.0))
+        theta_acc = math.atan2(-acc_x[i], math.sqrt(acc_y[i] ** 2.0 + acc_z[i] ** 2.0))
+        #[phi_acc, theta_acc] = gyro_x[i], gyro_y[i]
+        phi_offset += phi_acc
+        theta_offset += theta_acc
+        #sleep(sleep_time)
+
+    phi_offset = float(phi_offset) / float(N)
+    theta_offset = float(theta_offset) / float(N)
+
+    #print("Accelerometer offsets: " + str(phi_offset) + "," + str(theta_offset))
+
+
+    # Measured sampling time
+    dt = 0.0
+    #start_time = time()
+
+    #print("Running...")
+    for i in range(len(gyro_x)):
+
+        # Sampling time, 6e9 nanoseconds (or 6 seconds)
+        dt = 6.0
+        #start_time = time()
+
+        # Get accelerometer measurements and remove offsets
+        #[phi_acc, theta_acc] = imu.get_acc_angles()
+        phi_acc = math.atan2(acc_y[i], math.sqrt(acc_x[i] ** 2.0 + acc_z[i] ** 2.0))
+        theta_acc = math.atan2(-acc_x[i], math.sqrt(acc_y[i] ** 2.0 + acc_z[i] ** 2.0))
+        phi_acc -= phi_offset
+        theta_acc -= theta_offset
+        
+        # Gey gyro measurements and calculate Euler angle derivatives
+        [p, q, r] = gyro_x[i], gyro_y[i], gyro_z[i]
+        phi_dot = p + math.sin(phi_hat_arr[i]) * math.tan(theta_hat_arr[i]) * q + math.cos(phi_hat_arr[i]) * math.tan(theta_hat_arr[i]) * r
+        theta_dot = math.cos(phi_hat_arr[i]) * q - math.sin(phi_hat_arr[i]) * r
+        yaw_dot = math.sin(phi_hat_arr[i])*math.cos(theta_hat_arr[i])*q + math.sin(phi_hat_arr[i])*math.cos(theta_hat_arr[i])*r
+
+        # Kalman filter
+        # A = np.array([[1, 0, 0, -dt, 0, 0], [0, 1, 0, 0, -dt, 0], [0, 0, 1, 0 ,0, -dt], [0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 1] ])
+        # B = np.array([[dt, 0, 0], [0, dt, 0], [0, 0, dt], [0, 0, 0], [0, 0, 0], [0, 0, 0]])
+        A = np.array([[1, -dt, 0, 0], [0, 1, 0, 0], [0, 0, 1, -dt], [0, 0, 0, 1]])
+        B = np.array([[dt, 0], [0, 0], [0, dt], [0, 0]])
+
+        gyro_input = np.array([[phi_dot], [theta_dot]])
+        state_estimate = A.dot(state_estimate) + B.dot(gyro_input)
+        P = A.dot(P.dot(np.transpose(A))) + Q
+
+        measurement = np.array([[phi_acc], [theta_acc]])
+        y_tilde = measurement - C.dot(state_estimate)
+        S = R + C.dot(P.dot(np.transpose(C)))
+        K = P.dot(np.transpose(C).dot(np.linalg.inv(S)))
+        state_estimate = state_estimate + K.dot(y_tilde)
+        P = (np.eye(4) - K.dot(C)).dot(P)
+
+        phi_hat_arr[i] = state_estimate[0]
+        theta_hat_arr[i] = state_estimate[2]
+        #yaw_hat_arr[i] = state_estimate[3]
+
+        # Display results
+
+        #print("Phi: " + str(round(phi_hat_arr[i][0] * 180.0 / math.pi, 1)) + " Theta: " + str(round(theta_hat_arr[i][0] * 180.0 / math.pi, 1)) + " YAW: " + str(round(yaw_dot * 180.0 / math.pi, 1)) )
+
+    pitch_slope = np.mean(np.diff(phi_hat_arr))
+    roll_slope = np.mean(np.diff(theta_hat_arr))
+
+    if (math.isnan(pitch_slope)):
+        pitch_slope = 0.1
+    if (math.isnan(roll_slope)):
+        roll_slope = 0.1
+    #yaw_slope = np.mean(np.diff(acc_z))
+    return pitch_slope, roll_slope
+
+def preprocess_acc_data(acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, pitch_slope, roll_slope):
+
+    gyro_x_min = np.amin(gyro_x)
+    gyro_y_min = np.amin(gyro_y)
+    gyro_z_min = np.amin(gyro_z)
+
+    gyro_x_max = np.amax(gyro_x)
+    gyro_y_max = np.amax(gyro_y)
+    gyro_z_max = np.amax(gyro_z)
+
+
+
     x_min = np.amin(acc_x)
     y_min = np.amin(acc_y)
     z_min = np.amin(acc_z)
@@ -157,7 +354,7 @@ def preprocess_acc_data(acc_x, acc_y, acc_z):
     z_mmd = min_max_distance(acc_z)
 
     print("z_slope: ", z_slope)
-    return [x_min, y_min, z_min, x_max, y_max, z_max, x_std, y_std, z_std, x_mean, y_mean, z_mean, x_slope, y_slope, z_slope, x_zc, y_zc, z_zc, x_mmd, y_mmd, z_mmd]
+    return [gyro_x_min, gyro_y_min, gyro_z_min, gyro_x_max, gyro_z_max, gyro_z_max, x_min, y_min, z_min, x_max, y_max, z_max, x_std, y_std, z_std, x_mean, y_mean, z_mean, x_slope, y_slope, z_slope, x_zc, y_zc, z_zc, x_mmd, y_mmd, z_mmd,  pitch_slope, roll_slope]
 
 # Trains logistic regression on X_train and Y_train sets, predicts labels on X_test
 # and Y_test sets, dumps model to file and returns predicted labels
@@ -240,12 +437,17 @@ if __name__ == "__main__":
         ser.write(bytes(command.encode('ascii')))
         
         
-        x_acc, y_acc, z_acc = get_mult_acc_data(time_interval = 3, data_spacing = 0.001, fake=False)
+        x_acc, y_acc, z_acc, x_gyro, y_gyro, z_gyro = get_mult_acc_data(time_interval = 3, data_spacing = 0.001, fake=False)
         #print(x_acc, y_acc, z_acc)
 
         # preprocess the accelerometer data, and store into list format.
         # run_one_LR expects 2D-array. Adding another outer [] to make the 1D-array two dimensional.
-        X_test = [preprocess_acc_data(x_acc, y_acc, z_acc)]
+        pitch_slope, roll_slope = calc_attitude_data(x_acc, y_acc, z_acc, x_gyro, y_gyro, z_gyro)
+        print(pitch_slope)
+        print(roll_slope)
+       
+        X_test = [preprocess_acc_data(x_acc, y_acc, z_acc, x_gyro, y_gyro, z_gyro,  pitch_slope, roll_slope)]
+        
         #print(X_test)
         #print("len of X_train list: %s" % len(X_train))
 
@@ -342,7 +544,6 @@ if __name__ == "__main__":
             new_Y_FP = []
             new_Y_NP = []
             running_fps = 0
-
 
 
 
